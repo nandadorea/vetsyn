ee5171f2cf590b57c750cb94c06fe8459fdd4e0c
Fernanda Dorea <dorea.meyer@gmail.com>
2014-06-08 20:29:37 +0200

    fixing clean_baseline-perc (one function for two classes)

--- R/clean_baseline_percW.r
+++ R/clean_baseline_percW.r
@@ -1,73 +1,11 @@
-##' \code{clean_baseline_perc}
-##'
-##' Function to retrospectively remove possible outbreak signals and excessive
-##' noise, producing an outbreak free baseline that will serve to
-##' train outbreak-signal detection algorithms during prospective analysis.
-##' 
-##' The cleaning is non-parametric, based on moving
-##' percentiles. The user sets a window of time points, around each time point,
-##' which will be used to calculate the percentile set in the user in the argument
-##' limit. Any observations falling outside that percentile are removed
-##' and substituted by the percentile itself. See examples and references. See
-##' the package {caTools}, function runquantile() for details of
-##' how the running quantiles function handles the beginning and end of the
-##' time series.
-##'
 ##' @name clean_baseline_perc-methods
 ##' @docType methods
 ##' @seealso \code{\link{syndromicW}}
 ##' @aliases clean_baseline_perc
 ##' @aliases clean_baseline_perc-methods
 ##' @aliases clean_baseline_perc,syndromicW-method
-##'
-##' @param x a syndromicW object, which must have at least 
-##' the slot of observed data and a data frame in the slot dates.
-##' @param syndromes an optional parameter, if not specified, all
-##' columns in the slot observed of the syndromic object
-##' will be used. The user can choose to restriict the analyses to 
-##' a few syndromic groups listing their name or column position
-##' in the observed matrix. See examples.
-##' @param limit the percentile to be used in identifying outliers.
-##' @param run.window the number of time points to construct the moving
-##' percentile window. By default 120 days.
-##' @param plot whether plots comparing observed data and the result of 
-##' the cleaning process should be displayed.
-##' 
-##' @return An object of the class syndromicW which contains all 
-##' elements from the object provided in x, but in which
-##' the slot baseline has been filled with an outbreak-free baseline
-##' for each syndromic group. When the user chooses to restrict analyses to some 
-##' syndromes, the remaining columns are kept as is (if the slot was not empty)
-##' or filled with NAs when previously empty.
-##' 
-##' @keywords methods
 ##' @export
 ##' @importFrom caTools runquantile
-##' @references Fernanda C. Dorea, Crawford W. Revie, Beverly J. McEwen, 
-##' W. Bruce McNab, David Kelton, Javier Sanchez (2012). Retrospective 
-##' time series analysis of veterinary laboratory data: 
-##' Preparing a historical baseline for cluster detection in syndromic 
-##' surveillance. Preventive Veterinary Medicine. 
-##' DOI: 10.1016/j.prevetmed.2012.10.010.
-##' @examples
-##'data(lab.daily)
-##'my.syndromicW <- raw_to_syndromicW (id=SubmissionID,
-##'                                  syndromes.var=Syndrome,
-##'                                  dates.var=DateofSubmission,
-##'                                  date.format="%d/%m/%Y",
-##'                                  data=lab.daily)
-##'my.syndromicW <- clean_baseline_perc(my.syndromicW)
-##'my.syndromicW <- clean_baseline_perc(my.syndromicW,run.window=90)
-##'my.syndromicW <- clean_baseline_perc(my.syndromicW,
-##'                               syndromes="Musculoskeletal")
-##'my.syndromicW <- clean_baseline_perc(my.syndromicW,
-##'                               syndromes=c("GIT","Musculoskeletal"))
-##'my.syndromicW <- clean_baseline_perc(my.syndromicW,
-##'                               syndromes=3)
-##'my.syndromicW <- clean_baseline_perc(my.syndromicW,
-##'                               syndromes=c(1,3))
-
-
 
 setGeneric('clean_baseline_perc',
            signature = 'x',
